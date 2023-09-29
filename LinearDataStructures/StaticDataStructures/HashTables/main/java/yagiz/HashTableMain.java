edef757356a626dfd2cebf34795f96697524a27f
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-09-29 22:57:40 +0300

    HashTable package added and HashTable.txt file added

---
+++ LinearDataStructures/StaticDataStructures/HashTables/main/java/yagiz/HashTableMain.java
@@ -0,0 +1,17 @@
+package HashTables.main.java.yagiz;
+
+import java.util.Hashtable;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        Hashtable<String,String> table=new Hashtable<>();
+        table.put("a","A");
+        table.put("b","B");
+        table.put("c","C");
+        table.put("d","D");
+        table.put("e","E");
+        table.put("f","F");
+
+        System.out.println(table);
+    }
+}
