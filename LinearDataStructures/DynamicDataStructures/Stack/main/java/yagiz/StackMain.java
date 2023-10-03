85246b954c91b7efe1690ed25f5fe452a81956a8
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-03 22:24:33 +0300

    StackMain.java added, Stack.txt added

--- LinearDataStructures/DynamicDataStructures/Stack/main/java/yagiz/StackMain.java
+++ LinearDataStructures/DynamicDataStructures/Stack/main/java/yagiz/StackMain.java
@@ -1,5 +1,29 @@
 package LinearDataStructures.DynamicDataStructures.Stack.main.java.yagiz;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class StackMain {
-    
+    static Random rnd=new Random();
+    static Stack<Integer> textStack=new Stack<>();
+    public static void main(String[] args){
+        popStack(textStack);
+    }
+
+    public static void pushStack(Stack<Integer> stack){
+        for(int i=0;i<5;i++){
+            stack.push(rnd.nextInt(100));
+        }   
+    }
+
+    public static void popStack(Stack<Integer> stack){
+        int y=0;
+        for(int i=0;i<5;i++){
+            y= (Integer) stack.pop();
+            System.out.println(y);
+        }
+    }
+    public static void peekStack(Stack<Integer> stack){
+
+    }
 }
