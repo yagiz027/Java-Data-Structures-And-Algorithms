d544b79db20fe88fd738ad3b06e26aed87888c6d
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-03 22:26:40 +0300

    Queue.txt is edited, Stack.txt is edited and StackMain.java: Stack Operations added

--- LinearDataStructures/DynamicDataStructures/Stack/main/java/yagiz/StackMain.java
+++ LinearDataStructures/DynamicDataStructures/Stack/main/java/yagiz/StackMain.java
@@ -1,29 +1,43 @@
 package LinearDataStructures.DynamicDataStructures.Stack.main.java.yagiz;
 
-import java.util.Random;
 import java.util.Stack;
 
 public class StackMain {
-    static Random rnd=new Random();
     static Stack<Integer> textStack=new Stack<>();
     public static void main(String[] args){
+        pushStack(textStack);
         popStack(textStack);
+        pushStack(textStack);
+        peekStack(textStack);
+        searchStack(textStack, 0);
+        searchStack(textStack, 3);
     }
 
     public static void pushStack(Stack<Integer> stack){
         for(int i=0;i<5;i++){
-            stack.push(rnd.nextInt(100));
+            stack.push(i);
         }   
     }
 
     public static void popStack(Stack<Integer> stack){
-        int y=0;
+        
+        System.out.println("Pop Operation:");
         for(int i=0;i<5;i++){
-            y= (Integer) stack.pop();
+            Integer y= (Integer) stack.pop();
             System.out.println(y);
         }
     }
     public static void peekStack(Stack<Integer> stack){
+        Integer element=(Integer) stack.peek();
+        System.out.println("Element on stack top: " + element);
+    }
 
+    public static void searchStack(Stack<Integer> stack,int element){
+        Integer pos=(Integer) stack.search(element);
+        
+        if(pos==-1){
+            System.out.println("Element not found");
+        }else
+            System.out.println("Element is found at position: " + pos);
     }
 }
