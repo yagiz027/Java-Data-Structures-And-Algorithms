96f20ed29b19a5e1b1992dafeffff64540e4ec45
Yağız ERİŞ <117725245+yagiz027@users.noreply.github.com>
2023-10-03 12:36:12 +0300

    Queue.txt edited and Queue operations added

--- LinearDataStructures/DynamicDataStructures/Queue/main/java/yagiz/QueueMain.java
+++ LinearDataStructures/DynamicDataStructures/Queue/main/java/yagiz/QueueMain.java
@@ -1,5 +1,21 @@
 package LinearDataStructures.DynamicDataStructures.Queue.main.java.yagiz;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class QueueMain {
-    
+    public static void main (String[] args){
+        Queue<String> queueList=new PriorityQueue<>();
+        queueList.add("A"); //Kuyruğa bir eleman ekler eğer kuyruk dolu ise bir istisna fırlatır.
+        queueList.add("B"); 
+        queueList.offer("C");
+        queueList.add("D");
+        queueList.add("E");
+
+        String front=queueList.remove(); //Kuyruğun başından bir elemenı siler. Eğer içerine parametre olarak bir eleman girersek, belirtilen elemanı siler.
+
+        System.out.println("Removed Element: "+ front);
+        System.out.println("New Queue after removing operation: " + queueList);
+    }
+
 }
